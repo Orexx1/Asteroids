1a2fffc198f7f9be4dc3d66d6e19ff33e0d630bb
Orexx1 <dustin.m117@gmail.com>
2025-03-15 21:17:54 -0700

    Added player triangle code

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame 
 from constants import *
+from player import Player
+import circleshape
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0 
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     print ("Starting Asteroids!")
     print ("Screen width: 1280")
     print ("Screen height: 720")
@@ -17,11 +20,9 @@ def main():
                return
         dt = clock.tick(60) / 1000   
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
     
-
-
-
 
 if __name__ == "__main__":
     main()
