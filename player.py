a020a748bd7659667dc6bcd2b32067bf20fa0ea1
Orexx1 <dustin.m117@gmail.com>
2025-03-20 00:59:44 -0700

    Added rate limit to player shooting speed

--- player.py
+++ player.py
@@ -5,12 +5,14 @@ from constants import PLAYER_TURN_SPEED
 from constants import PLAYER_SPEED
 from constants import PLAYER_SHOOT_SPEED
 from constants import SHOT_RADIUS
+from constants import PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
      def __init__(self, x, y):
         super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation = 0 
+        self.shoottimer = 0 
      def draw(self, screen):
          pygame.draw.polygon(screen, "white", self.triangle(), 2)
      def rotate(self, dt):
@@ -22,7 +24,7 @@ class Player(CircleShape):
 
      def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.shoottimer -= dt  
 
         if keys[pygame.K_a]:
              self.rotate(-dt)
@@ -47,9 +49,14 @@ class Player(CircleShape):
         return [a, b, c]    
 
      def shoot(self): 
-         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-         shot_position = self.position + forward * self.radius  
-         new_shot = Shot(shot_position.x, shot_position.y, SHOT_RADIUS)
-         new_shot.velocity = forward * PLAYER_SHOOT_SPEED
-         return new_shot
+         if self.shoottimer <= 0:
+          forward = pygame.Vector2(0, 1).rotate(self.rotation)
+          shot_position = self.position + forward * self.radius  
+          new_shot = Shot(shot_position.x, shot_position.y, SHOT_RADIUS)
+          new_shot.velocity = forward * PLAYER_SHOOT_SPEED
+          self.shoottimer += PLAYER_SHOOT_COOLDOWN
+         
+          return new_shot
+         else:
+          return None
      
